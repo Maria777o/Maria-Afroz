1a74e9e0db676082a1d302ec64ec964b625defb8
Maria777o <mariaafroz20pad007@gmail.com>
2024-06-27 17:33:53 +0600

    Add files via upload

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<title>Edge Home</title>
+	<link rel="stylesheet" href="style.css">
+
+	</head>
+	<body>
+
+		<div class="header">
+		  <h1>EDGE: BU-CSE Digital Skills Training Program</h1>
+		</div>
+
+		<div class="topnav">
+		  <a href="index.php">Announcement</a>
+		  <a href="courses.php">Offered Courses</a>
+		  <a href="application.php">Apply Online</a>
+		</div>
+		
+		
+		
+		
